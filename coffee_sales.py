3cd5790f2e8312aaf10b1210d811c82030c0f344
Sabin Pantha <121206531+Saw1b@users.noreply.github.com>
2025-09-12 18:45:01 +0545

    coffee_sales.py

--- coffee_sales.py
+++ coffee_sales.py
@@ -8,7 +8,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from sklearn.preprocessing import StandardScaler
 
-# ========== CONFIG ==========
+# ========== CONFIG ==========#
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyDDjFc-eTL8MbCQp3EK96ulw6uTwT4ookQ")
@@ -126,3 +126,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
